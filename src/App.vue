1c3c29bfa2380085c22423222b0eaf636d1ac49d
Nabeel Ahsen <nabeelahsen.to@gmail.com>
2020-04-04 15:26:30 -0400

    Add Home component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="scrolling-wrapper">
+    <Home/>
+    <Home/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home,
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  position: absolute;
+}
+.scrolling-wrapper {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="home">
+    henlo
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+#home {
+  border-style: solid;
+  height: 100%;
+  width: 100vw;
+  flex: 0 0 auto;
+}
+</style>
